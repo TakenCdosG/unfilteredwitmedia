93e781235b1080e8d4064cb49f4f9fec87fc4861
Maicol Lopez Mora <maicol.lopez@akendos.com>
2015-12-23 13:25:50 -0500

    new subscribe

---
+++ wp-content/themes/founder-parent/subscribe.php
@@ -0,0 +1,40 @@
+<!--Begin CTCT Sign-Up Form-->
+<!-- EFD 1.0.0 [Wed Dec 23 12:20:09 EST 2015] -->
+<link rel='stylesheet' type='text/css' href='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/css/signup-form.css'>
+<div class="ctct-embed-signup" style="font: 16px Helvetica Neue, Arial, sans-serif; font: 1rem Helvetica Neue, Arial, sans-serif; line-height: 1.5; -webkit-font-smoothing: antialiased;">
+    <div >
+       <span id="success_message" style="display:none;">
+           <div style="text-align:center;">Thanks for signing up!</div>
+       </span>
+        <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.constantcontact.com/api/signup">
+            <h4>Subscribe to Unfiltered Wit Newsletter</h4>
+            <p></p>
+            <!-- The following code must be included to ensure your sign-up form works properly. -->
+            <input data-id="ca:input" type="hidden" name="ca" value="c127debf-b161-4b7b-a114-39104d258c13">
+            <input data-id="list:input" type="hidden" name="list" value="1">
+            <input data-id="source:input" type="hidden" name="source" value="EFD">
+            <input data-id="required:input" type="hidden" name="required" value="list,email">
+            <input data-id="url:input" type="hidden" name="url" value="">
+            <p data-id="Email Address:p" > <input data-id="Email Address:input" placeholder="Email Address" type="text" name="email" value="" maxlength="80"></p>
+            <button type="submit" class="Button ctct-button Button--block Button-secondary" data-enabled="enabled">Subscribe</button>
+        </form>
+    </div>
+</div>
+<script type="text/javascript">
+    var localizedErrMap = {};
+    localizedErrMap["required"] =   "This field is required.";
+    localizedErrMap["ca"] =    "An unexpected error occurred while attempting to send email.";
+    localizedErrMap["email"] =    "Please enter your email address in name@email.com format.";
+    localizedErrMap["birthday"] =   "Please enter birthday in MM/DD format.";
+    localizedErrMap["anniversary"] =  "Please enter anniversary in MM/DD/YYYY format.";
+    localizedErrMap["custom_date"] =  "Please enter this date in MM/DD/YYYY format.";
+    localizedErrMap["list"] =    "Please select at least one email list.";
+    localizedErrMap["generic"] =   "This field is invalid.";
+    localizedErrMap["shared"] =   "Sorry, we could not complete your sign-up. Please contact us to resolve this.";
+    localizedErrMap["state_mismatch"] = "Mismatched State/Province and Country.";
+    localizedErrMap["state_province"] = "Select a state/province";
+    localizedErrMap["selectcountry"] =  "Select a country";
+    var postURL = "https://visitor2.constantcontact.com/api/signup";
+</script>
+<script type="text/javascript" src="https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js"></script>
+<!--End CTCT Sign-Up Form-->
