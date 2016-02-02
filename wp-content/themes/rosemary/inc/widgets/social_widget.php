87580aba031196a32504770c0c2f68bd191a5227
Carlos Leaño <caedlesu@gmail.com>
2016-02-02 10:03:39 -0500

    added newsletter form to social widget sidebar and modified its styles

--- wp-content/themes/rosemary/inc/widgets/social_widget.php
+++ wp-content/themes/rosemary/inc/widgets/social_widget.php
@@ -70,6 +70,45 @@ class solopine_social_widget extends WP_Widget {
 				<?php if($vimeo) : ?><a href="http://vimeo.com/<?php echo get_theme_mod('sp_vimeo'); ?>" target="_blank"><i class="fa fa-vimeo-square"></i></a><?php endif; ?>
 				<?php if($linkedin) : ?><a href="<?php echo get_theme_mod('sp_linkedin'); ?>" target="_blank"><i class="fa fa-linkedin"></i></a><?php endif; ?>
 				<?php if($rss) : ?><a href="<?php echo get_theme_mod('sp_rss'); ?>" target="_blank"><i class="fa fa-rss"></i></a><?php endif; ?>
+        <link rel='stylesheet' type='text/css' href='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/css/signup-form.css'>
+<div class="ctct-embed-signup" style="font: 13px Helvetica Neue, Arial, sans-serif; font: 1rem Helvetica Neue, Arial, sans-serif; line-height: 1.5; -webkit-font-smoothing: antialiased;">
+   <div style="color:#5b5b5b; background-color:#e8e8e8; border-radius:5px;">
+       <span id="success_message" style="display:none;">
+           <div style="text-align:center;">Thanks for signing up!</div>
+       </span>
+       <form data-id="embedded_signup:form" class="ctct-custom-form Form" name="embedded_signup" method="POST" action="https://visitor2.constantcontact.com/api/signup">
+           <h2 style="margin:0;">Subscribe to Unfiltered Wit Newsletter</h2>
+           <p></p>
+           <!-- The following code must be included to ensure your sign-up form works properly. -->
+           <input data-id="ca:input" type="hidden" name="ca" value="c127debf-b161-4b7b-a114-39104d258c13">
+           <input data-id="list:input" type="hidden" name="list" value="1">
+           <input data-id="source:input" type="hidden" name="source" value="EFD">
+           <input data-id="required:input" type="hidden" name="required" value="list,email">
+           <input data-id="url:input" type="hidden" name="url" value="">
+           <p data-id="Email Address:p" ><label data-id="Email Address:label" data-name="email" class="ctct-form-required">Email Address</label> <input data-id="Email Address:input" type="text" name="email" value="" maxlength="80"></p>
+           <button type="submit" class="Button ctct-button Button--block Button-secondary" data-enabled="enabled">Sign Up</button>
+       	<div><p class="ctct-form-footer">By submitting this form, you are granting: 1000ThingsToSayBeforeIDie.com, 2243 Ibis Isle Road East, Palm Beach, Florida, 33480, United States, http://www.1000thingstosaybeforeidie.com permission to email you. You may unsubscribe via the link found at the bottom of every email.  (See our <a href="http://www.constantcontact.com/legal/privacy-statement" target="_blank">Email Privacy Policy</a> for details.) Emails are serviced by Constant Contact.</p></div>
+       </form>
+   </div>
+</div>
+<script type='text/javascript'>
+                                                                       var localizedErrMap = {};
+                                                                       localizedErrMap['required'] = 'This field is required.';
+                                                                       localizedErrMap['ca'] = 'An unexpected error occurred while attempting to send email.';
+                                                                       localizedErrMap['email'] = 'Please enter your email address in name@email.com format.';
+                                                                       localizedErrMap['birthday'] = 'Please enter birthday in MM/DD format.';
+                                                                       localizedErrMap['anniversary'] = 'Please enter anniversary in MM/DD/YYYY format.';
+                                                                       localizedErrMap['custom_date'] = 'Please enter this date in MM/DD/YYYY format.';
+                                                                       localizedErrMap['list'] = 'Please select at least one email list.';
+                                                                       localizedErrMap['generic'] = 'This field is invalid.';
+                                                                       localizedErrMap['shared'] = 'Sorry, we could not complete your sign-up. Please contact us to resolve this.';
+                                                                       localizedErrMap['state_mismatch'] = 'Mismatched State/Province and Country.';
+                                                                       localizedErrMap['state_province'] = 'Select a state/province';
+                                                                       localizedErrMap['selectcountry'] = 'Select a country';
+                                                                       var postURL = 'https://visitor2.constantcontact.com/api/signup';
+</script>
+<script type='text/javascript' src='https://static.ctctcdn.com/h/contacts-embedded-signup-assets/1.0.2/js/signup-form.js'></script>
+
 			</div>
 			
 			
